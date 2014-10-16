99a241324f4828451922b9c8f152183a33d4328c
ludovicbonivert <ludovic0_0@hotmail.com>
2014-10-16 18:55:44 +0200

    Version 0.1
    Completed the Unity tutorial for Space Shooter. I implemented new
    difficulty levels that isn’t in the tutorial version.

---
+++ Assets/Scripts/Coroutine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coroutine : MonoBehaviour {
+
+	void Start(){
+		StartCoroutine(exampleCorout());
+	}
+
+	IEnumerator exampleCorout(){
+		yield return new WaitForSeconds(2);
+		Debug.Log("Hello");
+		yield return new WaitForSeconds(2);
+		Debug.Log("Hello");
+	}
+}
